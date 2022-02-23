6c9704c33f28fd4208e3e50e3235f1e249d134cd
unknown <wevertoncamposdev@gmail.com>
2022-02-23 14:11:46 -0300

    teste index.php

--- index.php
+++ index.php
@@ -5,10 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>
-        <?php
-            $consurso = $_GET['consurso'];
-            echo ("Concurso: ".$consurso);
-        ?>
+        Histórico Lotofacil
     </title>
 </head>
 <body>
@@ -17,28 +14,32 @@
         Concurso: <input type="number" name="concurso" placeholder="Digite o N° do Concurso" required>
         <input type="submit" value="Concultar" name="button">
     </form>
-    
+
     <?php
+    
         $consurso = $_GET['consurso'];
-        $url =  "https://loteriascaixa-api.herokuapp.com/api/lotofacil/$consurso";
-        $data = file_get_contents($url);
-        $dataJson = json_decode($data);
-
-        //Nome
-        $nome = ($dataJson->nome);
-        //Data
-        $dia = ($dataJson->data);
-        //Concurso
-        $concurso = ($dataJson->concurso);
-        //Dezenas
-        $resultado = [];
-        foreach($dataJson->dezenas as $dezenas){
-            $resultado[] = $dezenas;
-        }
-        $dezenas = implode(',',$resultado);
-
-        echo("Nome: ". $nome . "Concurso: " . $concurso . "Data: " . $dia . "Dezenas: " . $dezenas);
+       
+        if($concurso){
+            $url =  "https://loteriascaixa-api.herokuapp.com/api/lotofacil/$consurso";
+            $data = file_get_contents($url);
+            $dataJson = json_decode($data);
+    
+            //Nome
+            $nome = ($dataJson->nome);
+            //Data
+            $dia = ($dataJson->data);
+            //Concurso
+            $concurso = ($dataJson->concurso);
+            //Dezenas
+            $resultado = [];
+            foreach($dataJson->dezenas as $dezenas){
+                $resultado[] = $dezenas;
+            }
+            $dezenas = implode(',',$resultado);   
+            echo("Nome: ". $nome . "Concurso: " . $concurso . "Data: " . $dia . "Dezenas: " . $dezenas);
         
+        }
+       
     ?>
 </body>
 </html>
