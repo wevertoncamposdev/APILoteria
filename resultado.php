121714a033beab526efb56328976477bba148ed4
unknown <wevertoncamposdev@gmail.com>
2022-02-23 15:14:52 -0300

    link css and create table

--- resultado.php
+++ resultado.php
@@ -1,25 +1,61 @@
-<?php
-    
-        $consurso = $_GET['concurso'];
-        
-            $url =  "https://loteriascaixa-api.herokuapp.com/api/lotofacil/$consurso";
-            $data = file_get_contents($url);
-            $dataJson = json_decode($data);
-    
-            //Nome
-            $nome = ($dataJson->nome);
-            //Data
-            $dia = ($dataJson->data);
-            //Concurso
-            $concurso = ($dataJson->concurso);
-            //Dezenas
-            $resultado = [];
-            foreach($dataJson->dezenas as $dezenas){
-                $resultado[] = $dezenas;
-            }
-            $dezenas = implode(',',$resultado);
+<!DOCTYPE html>
+<html lang="en">
 
-            echo("Nome: ". $nome . "Concurso: " . $concurso . "Data: " . $dia . "Dezenas: " . $dezenas);
-     
-       
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        <?php
+        $consurso = $_GET['concurso'];
+        echo ("Concurso: " . $consurso);
+        ?>
+    </title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <?php
+
+    $url =  "https://loteriascaixa-api.herokuapp.com/api/lotofacil/$consurso";
+    $data = file_get_contents($url);
+    $dataJson = json_decode($data);
+
+    //Nome
+    $nome = ($dataJson->nome);
+    //Data
+    $dia = ($dataJson->data);
+    //Concurso
+    $concurso = ($dataJson->concurso);
+    //Dezenas
+    $resultado = [];
+    foreach ($dataJson->dezenas as $dezenas) {
+        $resultado[] = $dezenas;
+    }
+    $dezenas = implode(',', $resultado);
+
     ?>
+
+    <table>
+        <tr>
+            <td><?php echo "Nome:" ?></td>
+            <td><?php echo $nome; ?></td>
+        </tr>
+        <tr>
+            <td><?php echo "Concurso:" ?></td>
+            <td><?php echo $concurso; ?></td>
+        </tr>
+        <tr>
+            <td><?php echo "Data:" ?></td>
+            <td><?php echo $dia; ?></td>
+        </tr>
+        <tr>
+            <td><?php echo "Dezenas:" ?></td>
+            <td><?php echo $dezenas; ?></td>
+        </tr>
+    </table>
+
+
+</body>
+
+</html>
