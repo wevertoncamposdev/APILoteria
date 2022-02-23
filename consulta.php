2b38f179fd04d08e0f3d07937a563631d6d03dcc
unknown <wevertoncamposdev@gmail.com>
2022-02-23 10:52:42 -0300

    atualização

--- consulta.php
+++ consulta.php
@@ -14,7 +14,25 @@
 <body>
     <?php
         $consurso = $_GET['consurso'];
-        echo $consurso;
+        $url =  "https://loteriascaixa-api.herokuapp.com/api/lotofacil/$consurso";
+        $data = file_get_contents($url);
+        $dataJson = json_decode($data);
+
+        //Nome
+        $nome = ($dataJson->nome);
+        //Data
+        $dia = ($dataJson->data);
+        //Concurso
+        $concurso = ($dataJson->concurso);
+        //Dezenas
+        $resultado = [];
+        foreach($dataJson->dezenas as $dezenas){
+            $resultado[] = $dezenas;
+        }
+        $dezenas = implode(',',$resultado);
+
+        echo("Nome: ". $nome . "Concurso: " . $concurso . "Data: " . $dia . "Dezenas: " . $dezenas);
+        
     ?>
 </body>
 </html>
